73d535ca8ac54d81aa6a0ec9d33eb7b00c6c185b
Galen Emery <galen@galenemery.com>
2015-04-15 13:42:38 -0700

    Cleaned up old cookbook

--- cookbooks/hyperv/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'hyperv'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures hyperv'
-long_description 'Installs/Configures hyperv'
-version          '0.1.0'
-
--- cookbooks/hyperv/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook Name:: hyperv
-# Recipe:: default
-#
-# Copyright (c) 2015 The Authors, All Rights Reserved.
